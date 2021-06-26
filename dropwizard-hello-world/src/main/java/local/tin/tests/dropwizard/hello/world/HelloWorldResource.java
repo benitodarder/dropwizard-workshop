e8e40b40cd89ac0aa5b6cce2301c1e45f8cd8a68
benitodarder@gmail.com <benitodarder@gmail.com>
2021-06-26 17:17:34 +0200

    Sample POST

--- dropwizard-hello-world/src/main/java/local/tin/tests/dropwizard/hello/world/HelloWorldResource.java
+++ dropwizard-hello-world/src/main/java/local/tin/tests/dropwizard/hello/world/HelloWorldResource.java
@@ -10,9 +10,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 
 @Path("/hello-world")
-@Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
     private final String defaultName;
 
@@ -21,7 +22,9 @@ public class HelloWorldResource {
     }
 
     @GET
+    @Path("/get")
     @Timed
+    @Produces(MediaType.APPLICATION_JSON)
     public HelloWorld sayHello(@QueryParam("name") Optional<String> name) {
         if (name.isPresent()) {
             return new HelloWorld(new Date(), getMessage(name.get()));
@@ -34,5 +37,17 @@ public class HelloWorldResource {
     private String getMessage(String name) {
         return "Hello " + name + ", it's " + new Date();
     }
+    
+    @POST
+    @Path("/post")
+    @Timed
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public HelloWorld sayHelloByPost(String name) {
+        if (!name.isBlank()) {
+            return new HelloWorld(new Date(), getMessage(name));
+        } else {
+            return new HelloWorld(new Date(), getMessage(defaultName));
+        }    }    
 }
 
